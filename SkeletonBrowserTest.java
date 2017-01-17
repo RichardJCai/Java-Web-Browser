c492fb3e947ed700f2cec2c29b08afc8e4cedf3d
RichardJCai <caioftherichard@gmail.com>
2017-01-16 20:46:09 -0500

    Uploaded Files

---
+++ SkeletonBrowserTest.java
@@ -0,0 +1,11 @@
+// Create and start a SkeletonBrowser.
+import javax.swing.JFrame;
+ 
+public class SkeletonBrowserTest
+{
+   public static void main( String args[] )
+   {
+      SkeletonBrowser application = new SkeletonBrowser();
+      application.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+   } // end main
+} // end class SkeletonBrowserTest
